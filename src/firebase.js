e83340d1e4b0979e7b611652c7d80c22289b3451
peyman iravani <peyman.iravani.altranitalia@windtre.it>
2020-04-08 00:28:59 +0200

    r

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,28 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
-const firebaseConfig = firebase.initializeApp({
+
+const config = {
   apiKey: process.env.apiKey,
   authDomain: process.env.authDomain,
   databaseURL: process.env.databaseURL,
   projectId: process.env.projectId,
   storageBucket: process.env.storageBucket,
-  messageSenderId: process.env.messageSenderId,
-  apiId: process.env.apiId,
+  messageSenderId: process.env.messageSenderId
+};
+const firebaseConfig = firebase.initializeApp(config);
+
+/*const firebaseConfig = firebase.initializeApp({
+  apiKey: 'AIzaSyB-Lb1ZJ6WM2_ZMuAA-eJadVMeq7NuSKz0',
+  authDomain: 'dispache-4b5a7.firebaseapp.com',
+  databaseURL: 'https://dispache-4b5a7.firebaseio.com',
+  projectId: 'dispache-4b5a7',
+  storageBucket: 'dispache-4b5a7.appspot.com',
+  messageSenderId: '827819004463',
+  apiId: '1:827819004463:web:f1866d64c38265b28c6cda',
 
 
 
 
 });
-
+*/
 export { firebaseConfig as firebase };
